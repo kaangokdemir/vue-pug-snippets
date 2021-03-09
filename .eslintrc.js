305261b413644f8504eb71b92684040270be1d89
Kaan <kaangokdemir01@gmail.com>
2021-03-09 21:49:51 +0100

    chore: prettier and eslint configiration added

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ['standard'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-template': ['error'],
+    camelcase: 'off',
+    'no-control-regex': 'off'
+  }
+}
--- extension.ts
+++ extension.ts
@@ -1,4 +1,4 @@
-export function activate(context) {
+export function activate (context) {
   console.log(
     'Congratulations, your extension "vue-pug-snippets" is now active!'
   )
